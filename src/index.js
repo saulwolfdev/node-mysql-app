3c086628a1c90c60b9a9faf5d8b66e1caa38c35d
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-10-26 17:15:09 -0300

    merge

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
 const express = require("express");
-const morgan=require("morgan");
-const exphbs=require("express-handlebars");
-const path = require("path")
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
 //initializations
-const app = express()
+const app = express();
 //settings
-app.set("port",process.env.PORT|| 4000)
-app.set("views",path.join(__dirname,"views"))
+app.set("port", process.env.PORT || 4000);
+app.set("views", path.join(__dirname, "views"));
 app.engine(
   ".hbs",
   exphbs({
     defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
     partialsDir: path.join(app.get("views"), "partials"),
-    extname:".hbs",
-    helpers:require("./lib/handlebars")
+    extname: ".hbs",
+    helpers: require("./lib/handlebars")
   })
 );
-app.set("view engine","hbs");
+app.set("view engine", "hbs");
+
 //middlewares
 app.use(morgan("dev"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //global variables
-app.use((req, res, next)=>{
-    next();
+app.use((req, res, next) => {
+  next();
 });
-//routes
-app.use(require("./routes"))
-app.use(require("./routes/authentications"));
-app.use("/links",require("./routes/links"));
 
-app.use(express.static(path.join(__dirname,"public")));
+//routes
+app.use(require("./routes"));
+app.use(require("./routes/authentications"));
+app.use("/links", require("./routes/links"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 //public
 
 //startin thee server
- app.listen(app.get("port"),()=>{
-     console.log("SERVER ON PORT", app.get("port"))
- })
+app.listen(app.get("port"), () => {
+  console.log("SERVER ON PORT", app.get("port"));
+});
